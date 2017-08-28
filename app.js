0a324606e9141ef34e36eeb2830796e7da9b0011
Edward Dupre <edwd42@gmail.com>
2017-08-28 17:03:07 +0000

    complete challenge 1.3 Locations

---
+++ app.js
@@ -0,0 +1,20 @@
+// 1.3 Locations 250 pts
+
+// Let's start coding our Express application.
+// Task 1/6 In our app.js, require the express module and assign it to the express variable.
+// Task 2/6 Using the function assigned to express, create an application instance and assign it to the app variable.
+// Task 3/6 Using our application instance, app, create a new route that accepts GET requests on the /locations URL path. Remember to pass a callback function which takes a request and response.
+// Task 4/6 Respond with an array of city names. The city names should be Caspiana, Indigo and Paradise.
+// Task 5/6 Bind our application to port 3001.
+// Task 6/6 When our application is ready to receive requests, print the string "Running Express" to the console.
+
+var express = require('express');
+var app = express();
+
+app.get('/locations', function(request, response){
+  var city = ['Caspiana', 'Indigo', 'Paradise'];
+  response.send(city);
+}).listen(3001, function(){
+  console.log("Running Express");
+});
+
