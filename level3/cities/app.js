d05247afbe66ccec15f9177d2692d0a11b8bd2d4
Edward Dupre <edwd42@gmail.com>
2017-08-29 17:13:42 +0000

    complete challenge 3.9 Dynamic Routes III

--- level3/cities/app.js
+++ level3/cities/app.js
@@ -1,39 +1,43 @@
-// 3.8 Dynamic Routes II 250 pts
+// 3.9 Dynamic Routes III 250 pts
 
-// Whenever we use our name parameter we want to parse it a specific way. Let's clean up our existing code so that all routes with a name parameter get the same special handling.
-// Task 1/3 Call app.param() to intercept requests that contain an argument called 'name'. Remember app.param() takes a callback function as its second argument, which uses the same signature as a middleware.
-// Task 2/3 Inside the app.param() callback function, call the parseCityName() function with the submitted name parameter. Set the return value to a new property in the request object called cityName.
-// Task 3/3 Finally, call a function that moves processing to the next function in the stack.
+// The following code has a Dynamic Route that takes a year as an argument and returns the city created in that year. The problem with our current implementation is that it breaks when invalid data is sent on client requests. Let's add some basic validation.
+// Task 1/3 Call a function that intercepts Dynamic Routes with the 'year' param.
+// Task 2/3 Inside of that function, use the isYearFormat() function to check whether the year parameter is in a valid format. If so, then move processing to the next function in the stack.
+// Task 3/3 If the year parameter is not in a valid format, then respond with a 400 HTTP status code and a JSON message 'Invalid Format for Year'.
 
 var express = require('express');
 var app = express();
 
-var cities = {
-  'Lotopia': 'Rough and mountainous',
-  'Caspiana': 'Sky-top island',
-  'Indigo': 'Vibrant and thriving',
-  'Paradise': 'Lush, green plantation',
-  'Flotilla': 'Bustling urban oasis'
-};
-
-app.param('name', function(request, response, next){
-  var name = parseCityName(request.params.name);
-  request.cityName = name;
-  next();
+app.param('year', function(request, response, next){
+    if(isYearFormat(request.params.year)){
+      next();
+     } else {
+       response.status(400).json('Invalid Format for Year');
+     }
 });
 
-app.get('/cities/:name', function (request, response) {
-  var cityInfo = cities[request.cityName];
-  if(cityInfo) {
-    response.json(cityInfo);
+var citiesYear = {
+  5000: 'Lotopia',
+  5100: 'Caspiana',
+  5105: 'Indigo',
+  6000: 'Paradise',
+  7000: 'Flotilla'
+};
+
+function isYearFormat(value) {
+  var regexp = RegExp(/^d{4}$/);
+  return regexp.test(value);
+}
+
+app.get('/cities/year/:year', function(request, response) {
+  var year = request.params.year;
+  var city = citiesYear[year];
+
+  if(!city) {
+    response.status(404).json("No City found for given year");
   } else {
-    response.status(404).json("City not found");
+    response.json("In " + year + ", " + city + " is created.");
   }
 });
 
-function parseCityName(name){
-  var parsedName = name[0].toUpperCase() + name.slice(1).toLowerCase();
-  return parsedName;
-}
-
-app.listen(3000);                                                                                                                                                                                                                                                                                                            
+app.listen(3000);                                                                                                                                                                                                                                                                                                       
