25560e55381f201d01880a06e30596c171559c33
Edward Dupre <edwd42@gmail.com>
2017-08-29 02:17:25 +0000

    complete challenge 2.9 Only GET

--- level2/video_notes.js
+++ level2/video_notes.js
@@ -1,3 +1,27 @@
+// level 2 How Middleware Works
+var express = require('express');
+var app = express();
+
+app.get('/', function(request, response){
+    response.sendFile(__dirname + '/public/index.html');
+}).listen(process.env.POST);
+
+// using express.static middleware
+app.use(express.static('public'));
+
+// when next() is called, processing moves to the next middleware
+app.use(function(request, response, next){
+    // ...
+    next();
+})
+
+app.use(function(request, response, next){
+    // ...
+    response.send('done!');
+    next();  // calling next after the response has completed will cause errors
+})
+
+
 // node.js module system follows the CommonJS specification
 // in logger.js
 module.exports = function(request, response, next){
