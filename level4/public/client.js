2264d1f470fcb72d06d33ade0755de25989a38d3
Edward Dupre <edwd42@gmail.com>
2017-08-30 00:40:09 +0000

    complete challenge 4.2 Parser Setup

---
+++ level4/public/client.js
@@ -0,0 +1,33 @@
+$(function(){
+
+  $.get( '/blocks', appendToList); 
+
+  function appendToList(blocks) {
+    var list = [];
+    var content, block;
+    for(var i in blocks){
+      block = blocks[i];
+      // link to eac block's description
+      content = '<a href="/blocks/'+block+'">'+block+'</a>';
+      list.push($('<li>', { html: content }));
+    }
+    
+    $('.block-list').append(list);
+  }
+  
+  $('form').on('submit', funvtion(event){
+    event.preventDefault();
+    var form = $(this);
+    // transforms the form tada to URL-encoded notation
+    var blockData = form.serialize();
+    
+    $.ajax({
+      type: 'POST', url: '/blocks',  data: blockData
+    }).done(function(blockname){
+      // array with the new block as its single argument
+      appendToList([blockName]);
+      // clean up form text input fields
+      form.trigger('reset');
+    });
+  });
+});
