81bc40e8c8b1b2558eb58ef4a406d57b45755907
Edward Dupre <edwd42@gmail.com>
2017-09-01 01:52:53 +0000

    complete challenge 4.6 Delete Route

--- level4/app.js
+++ level4/app.js
@@ -20,3 +20,44 @@ app.post('/cities', parseUrlencoded, function (request, response) {
 });
 
 app.listen(3000);
+
+// 4.6 Delete Route 250 pts
+
+// Create a Dynamic Route for deleting cities and handle for cities that are not in our list.
+// Task 1/4 Create a DELETE route that takes the city name as its first argument, followed by a callback that takes a request and response objects as arguments.
+// Task 2/4 Use the built-in JavaScript operator delete (see MDN docs) to remove the property for the city passed as an argument. Don't forget to use the attribute set in app.param() to look the city up. 
+// Task 3/4 Use sendStatus() to respond back with a status code of 200.
+// Task 4/4 Add an if block that checks whether the cityName provided from app.param() has a valid entry before attempting to delete it from the cities object. If a valid city is not found, then respond with a 404 HTTP status code using the sendStatus() function.
+
+var express = require('express');
+var app = express();
+
+var cities = {
+  'Lotopia': 'Rough and mountainous',
+  'Caspiana': 'Sky-top island',
+  'Indigo': 'Vibrant and thriving',
+  'Paradise': 'Lush, green plantation',
+  'Flotilla': 'Bustling urban oasis'
+};
+
+app.param('name', function (request, response, next) {
+  request.cityName = parseCityName(request.params.name);
+});
+       
+app.delete('/cities/:name', function(request, response){
+  if(cities[request.cityName]){
+    delete cities[request.cityName];
+    response.sendStatus(200);
+  } else {
+    response.sendStatus(404);
+  }
+
+});
+
+app.listen(3000);
+
+function parseCityName(name) {
+  var parsedName = name[0].toUpperCase() + name.slice(1).toLowerCase();
+  return parsedName;
+}
+
