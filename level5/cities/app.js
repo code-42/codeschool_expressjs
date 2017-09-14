ec5713b3ee0f63783e4ace9577020ffab5107eb4
Edward Dupre <edwd42@gmail.com>
2017-09-14 20:35:47 +0000

    complete Level 5 Part 2 challenge

--- level5/cities/app.js
+++ level5/cities/app.js
@@ -1,81 +1,100 @@
 /*
-5.2 Route Instance 250 pts
+5.4 Using a Router Instance 250 pts
 
-Let's rewrite our cities routes using a Route Instance.
+Let's refactor app.js to use a Router object.
 
-Create a new Route Instance for the '/cities' URL path and assign it to the citiesRoute variable.
-Move the code from our previous app.get() route to a new GET route on the citiesRoute object.
-Move app.post() to citiesRoute.
-Now, let's get rid of the citiesRoute temporary variable and use chaining function calls.
-Finally, let's move the old routes for the '/cities/:name' URL path to use the new Route Instance API.
+Create a new router object and assign it to the router variable.
+When we are done, our router will be mounted on the /cities path. With this in mind, change app.route('/cities') to use router and map requests to the root path.
+Likewise, let's move our '/cities/:name' route to our router. Remember to update the path.
+Our router is now ready to be used by app. Mount our new router under the /cities path.
 
 */
 
 var express = require('express');
 var app = express();
+
 var bodyParser = require('body-parser');
 var parseUrlencoded = bodyParser.urlencoded({ extended: false });
-// In memory store for the cities in our application
-var cities = {};
 
-app.route('/cities')
+// In memory store for the
+// cities in our application
+var cities = {
+  'Lotopia': 'Rough and mountainous',
+  'Caspiana': 'Sky-top island',
+  'Indigo': 'Vibrant and thriving',
+  'Paradise': 'Lush, green plantation',
+  'Flotilla': 'Bustling urban oasis'
+};
 
-// GET route for /cities
-.get(function (request, response) {
-  if(request.query.search) {
-    response.json(citySearch(request.query.search));
-  } else {
-    response.json(cities);
-  }
-})
-
-// POST route for /cities
-.post(parseUrlencoded, function (request, response) {
-  if(request.body.description.length > 4) {
-    var city = createCity(request.body.name, request.body.description);
-    response.status(201).json(city);
-  } else {
-    response.status(400).json('Invalid City');
-  }
+app.param('name', function (request, response, next) {
+  request.cityName = parseCityName(request.params.name);
 });
 
-// GET route for /cities/:name
-app.route('/cities/:name')
-.get(function (request, response) {
-  var cityInfo = cities[request.cityName];
-  if(cityInfo) {
-    response.json(cityInfo);
-  } else {
-    response.status(404).json('City not found');
-  }
-})
+var router = express.Router();
 
-// DELETE route for /cities/:name
-.delete(function (request, response) {
-  if(cities[request.cityName]) {
-    delete cities[request.cityName];
-    response.sendStatus(200);
-  } else {
-    response.sendStatus(404);
-  }
-});
-
-// Searches for keyword in description and returns the city
-function citySearch(keyword) {
-  var result = null;
-  var search = RegExp(keyword, 'i');
-  for(var city in cities) {
-    if(search.test(cities[city])) {
-      return city;
+router.route('/')
+  .get(function (request, response) {
+    if(request.query.search){
+      response.json(citySearch(request.query.search));
+    }else{
+      response.json(cities);
     }
-  }
+  })
+
+  .post(parseUrlencoded, function (request, response) {
+    if(request.body.description.length > 4){
+      var city = createCity(request.body.name, request.body.description);
+      response.status(201).json(city);
+    }else{
+      response.status(400).json('Invalid City');
+    }
+  });
+
+router.route('/:name')
+  .get(function (request, response) {
+    var cityInfo = cities[request.cityName];
+    if(cityInfo){
+      response.json(cityInfo);
+    }else{
+      response.status(404).json("City not found");
+    }
+  })
+
+  .delete(function (request, response) {
+    if(cities[request.cityName]){
+      delete cities[request.cityName];
+      response.sendStatus(200);
+    }else{
+      response.sendStatus(404);
+    }
+  });
+
+app.use('/cities', router);
+
+// Searches for keyword in description
+// and returns the city
+function citySearch(keyword) {
+  var regexp = RegExp(keyword, 'i');
+  var result = cities.filter(function (city) {
+    return city.match(regexp);
+  });
+
+  return result;
 }
 
-// Adds a new city to the in memory store
-function createCity(name, description) {
+// Adds a new city to the
+// in memory store
+function createCity(name, description){
   cities[name] = description;
   return name;
 }
 
-app.listen(3000);
+// Uppercase the city name.
+function parseCityName(name){
+  var parsedName = name[0].toUpperCase() + name.slice(1).toLowerCase();
+  return parsedName;
+}
 
+app.listen(process.env.PORT, function() {
+    console.log('Listening on port ' + process.env.PORT + '\n');
+});
