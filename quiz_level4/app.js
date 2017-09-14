7f260f3d50856a981b9a3a495cec082e20949fc2
Edward Dupre <edwd42@gmail.com>
2017-09-14 03:23:45 +0000

    add form validation

--- quiz_level4/app.js
+++ quiz_level4/app.js
@@ -26,12 +26,8 @@ Form should confirm that lat lon are numbers.
 var express = require('express');
 var app = express();
 
-// var bodyParser = require('body-parser');
-
-const bodyParser = require('body-parser');
-// app.use(bodyParser);
-
-var parseUrlencoded = bodyParser.urlencoded({extended: false});
+var bodyParser = require('body-parser');
+var parseUrlencoded = bodyParser.urlencoded({ extended: false });
 
 app.use(express.static('public'));
 
@@ -44,8 +40,8 @@ var cities = {
     'Hell': 'Michigan'
 };
 
-console.log("32. cities.length == " + Object.keys(cities).length);
-console.log("33. cities == " + Object.keys(cities));
+console.log("43. cities.length == " + Object.keys(cities).length);
+console.log("44. cities == " + Object.keys(cities));
 // console.log("34. states == " + Object.values(cities));
 
 // A /cities route that will display all cities. (minimum of 5 cities)
@@ -54,66 +50,64 @@ console.log("33. cities == " + Object.keys(cities));
 // All cities if 0 is provided or if limit query is omitted
 // return a status error if the limit is higher than the number of cities available in the list
 
-app.get('/cities', function(request, response){
-  if(request.query.limit > Object.keys(cities).length){
-      console.log("26.cities.length == " + Object.keys(cities).length + ", you requested " + request.query.limit);
-      response.status(400).json('Error 400. Limit is higher than the number of cities available in the list');
-      response.end();
-  } else if(request.query.limit >= 1){
-      console.log("\n30.request.limit == " + request.query.limit);
-      var keys = [];
-      for (key in cities){
-          keys.push(key);
-      }
-      response.json(keys.slice(0, request.query.limit));
-  }
-  else {
-      console.log("56.request.limit is 0 or omitted");
-      response.json(Object.keys(cities));
-  }
+app.get('/cities', function(request, response) {
+    if (request.query.limit > Object.keys(cities).length) {
+        console.log("26.cities.length == " + Object.keys(cities).length + ", you requested " + request.query.limit);
+        response.status(400).json('Error 400. Limit is higher than the number of cities available in the list');
+        response.end();
+    } else if (request.query.limit >= 1) {
+        console.log("\n30.request.limit == " + request.query.limit);
+        var key = '';
+        var keys = [];
+        for (key in cities) {
+            console.log("63. key == " + key);
+            keys.push(key);
+        }
+        response.json(keys.slice(0, request.query.limit));
+    } else {
+        console.log("56.request.limit is 0 or omitted");
+        response.json(Object.keys(cities));
+    }
 });
 
 // Make sure to also normalize the data sent in the /cities route. The city sent should be sendable in any case and still find the state it’s in. ie Providence and providence should both return Rhode Island.
-app.param('name', function(request, response, next){
+app.param('name', function(request, response, next) {
     var name = request.params.name;
     var city = name[0].toUpperCase() + name.slice(1).toLowerCase();
-    console.log("80. city == ", city);
     request.cityName = city;
-    console.log("66. == ", request.cityName);
+    console.log("78. == " + request.cityName);
     next();
 });
 
 // Add a dynamic route to /cities. This should respond with the state that the city resides in.
-app.get('/cities/:name', function(request, response){
+app.get('/cities/:name', function(request, response) {
     var state = cities[request.cityName];
-    // console.log("89. == " + cities[request.cityName]);
-    console.log("89. == " + state);
+    // console.log("73. == ", cities[request.cityName]);
     // document.getElementById('state').value = state;
     // $('#state').html(state);
-    if(!state){
+    if (!state) {
         response.status(404).json('No state found for ' + request.params.name);
     }
-    console.log("95. == " + cities[request.cityName]);
-    response.json(JSON.stringify(request.body);
-    
+    response.json(state);
+
 });
 
-app.post('/', parseUrlencoded, function(request, response){
-    // var newCity = request.body;
-    // cities[newCity.description] = newCity.description;
-    // response.status(201).json(cities[newCity.description]);
-    var newCity = request.body;
-    console.log("103. " + newCity.city);
-    cities[newCity.state] = newCity.state;
-    console.log("105. " + cities[newCity.state]);
-    response.status(201).json(cities[newCity.city]);
-});
+// app.post('/', parseUrlencoded, function(request, response) {
+//     var newCity = request.body;
+//     console.log("103. " + newCity.city);
+//     cities[newCity.state] = newCity.state;
+//     console.log("105. " + cities[newCity.state]);
+//     response.status(201).json(cities[newCity.city]);
+// });
 
-app.post('/cities', parseUrlencoded, function(request, response){
+app.post('/cities', parseUrlencoded, function(request, response) {
     var newCity = request.body;
     console.log("111. " + JSON.stringify(newCity.city));
     console.log("112. " + JSON.stringify(newCity.state));
-    cities[newCity.city] = newCity;
+    cities[newCity.city] = newCity.city;
+    cities[newCity.state] = newCity.state;
+    // console.log("108. " + cities[newCity.city]);
+    // console.log("109. " + cities[newCity.state]);
     // cities[newCity.state] = JSON.stringify(newCity.state);
     console.log("115. " + JSON.stringify(cities));
     console.log("116. " + JSON.stringify(request.body));
@@ -121,8 +115,8 @@ app.post('/cities', parseUrlencoded, function(request, response){
     response.status(201).json(newCity.city);
 });
 
-app.listen(process.env.PORT, function(){
-    console.log('Listening on port ' + process.env.PORT);
+app.listen(process.env.PORT, function() {
+    console.log('Listening on port ' + process.env.PORT + '\n');
 });
 
-// response.write(JSON.stringify(request.body));
+// app.listen(8000);
--- quiz_level4/public/client.js
+++ quiz_level4/public/client.js
@@ -1,8 +1,8 @@
 // quiz level 4
-$(function(){
-console.log("2.found client.js");
+$(function() {
+    console.log("2.found client.js");
 
-  $.get( '/cities', appendToList); 
+    $.get('/cities', appendToList);
 
     function appendToList(cities) {
         var list = [];
@@ -11,29 +11,32 @@ console.log("2.found client.js");
             city = cities[i];
             // link to eac block's description
             content = '<a href="/cities/' + city + '">' + city + '</a>';
-            console.log("15. content == " + content);
             // list.push($('<li>', { html: content }));
             list.push($('<li>' + content + '</li>'));
         }
         $('#city-list').html(list);
     };
-    
-  $('form').on('submit', function(event){
-    event.preventDefault();
-    var form = $(this);
-    // transforms the form tada to URL-encoded notation
-    var cityData = form.serialize();
-    console.log("26. cityData == " + cityData);
-    
-    $.ajax({
-      type: 'POST', url: '/cities',  data: cityData
-    }).done(function(cityName){
-      console.log("31. cityName == " + cityName);
-      console.log("32. cityData == " + cityData);
-      appendToList([cityName]);
-      appendToList([cityData]);
-      // clean up form text input fields
-      form.trigger('reset');
+
+    $('form').on('submit', function(event) {
+        event.preventDefault();
+        console.log("22. inside .on");
+        var form = $(this);
+        // transforms the form tada to URL-encoded notation
+        var cityData = form.serialize();
+        console.log("26. cityData == " + cityData);
+
+        $.ajax({
+            type: 'POST',
+            url: '/cities',
+            data: cityData
+        }).done(function(cityName) {
+            console.log("33. cityName == " + cityName);
+            console.log("34. cityData == " + cityData);
+            appendToList([cityName]);
+            // appendToList([cityData]);
+            // clean up form text input fields
+            form.trigger('reset');
+        });
+        console.log("39. add city " + cityData);
     });
-  });
-});  
+});
